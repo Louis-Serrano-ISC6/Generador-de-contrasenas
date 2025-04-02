b0e5427cc9daaa0868fbc6af26875af2486f6809
Louis Alamo <louisgerardoserranoalamo@gmail.com>
2025-04-02 09:06:59 -0600

    change name and imports

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from prueba-contrasena import validar_contrasena
+from prueba_contrasena import validar_contrasena
+
 
 if __name__ == "__main__":
     if validar_contrasena("Contraseña123!"):
         print("Contraseña válida")
     else:
         print("Contraseña inválida")
+
+
